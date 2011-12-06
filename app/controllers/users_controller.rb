ef5315862e42ad92d641f2c6d6b6b229765e3ee0
JNAranda <joearanda@gmail.com>
2011-12-06 11:22:33 -0500

    pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
 
   def new
-    @title = "Sign up"
+    @title = "Start Reading"
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      @title = "Start Reading"
+      render = 'new'
+    end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Joseph Aranda"
+  user.email                 "joe@example.com"
+  user.password              "sixletter"
+  user.password_confirmation "sixletter"
+end
